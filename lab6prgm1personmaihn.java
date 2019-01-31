dc3273704cb16c698eb23d38083dbf86b604dfa5
yashsukhwanicr07 <35224859+yashsukhwanicr07@users.noreply.github.com>
2019-01-31 14:05:18 +0530

    Create lab6prgm1personmaihn.java

---
+++ lab6prgm1personmaihn.java
@@ -0,0 +1,12 @@
+package com.cg_vip.lab1;
+
+public class PersonMain {
+
+	public static void main(String[] args) throws MyException
+	{
+			Person p1 = new Person("Bharti","Bharti", 'F');
+			
+
+	}
+
+}
